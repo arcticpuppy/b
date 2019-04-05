3fa0172a58d738d69141dfe76270a07c974a5ddd
arcticpuppy <37775849+arcticpuppy@users.noreply.github.com>
2019-04-05 23:03:22 +0800

    Add files via upload

---
+++ UNetKeras.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# Author:ZERO
+# E-mail:zero@osai.club
+# Create Date: 2018年5月24日
+
+
+import tensorflow as tf
+
+import numpy as np
+
+
+class UNetKeras(object):
+
+    def __init__(self, height=512, width=512, channel=1, classes=3):
+        """
+        U-net
+
+        :param height int: The height of the picture
+        :param width int: The width of the picture
+        :param channel int: The number of channels in the picture,default is 1
+        :param classes int: Classification number
+        """
+        inputs = tf.keras.layers.Input((height, width, channel))
+        conv1 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+        pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+
+        conv2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+        conv2 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+        pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+
+        conv3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+        conv3 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+        pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+
+        conv4 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+        conv4 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+        drop4 = tf.keras.layers.Dropout(0.5)(conv4)
+        pool4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(drop4)
+
+        conv5 = tf.keras.layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(
+            pool4)
+        conv5 = tf.keras.layers.Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(
+            conv5)
+        drop5 = tf.keras.layers.Dropout(0.5)(conv5)
+
+        up6 = tf.keras.layers.Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            tf.keras.layers.UpSampling2D(size=(2, 2))(drop5))
+        merge6 = tf.keras.layers.Concatenate(axis=3)([drop4, up6])
+        conv6 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(
+            merge6)
+        conv6 = tf.keras.layers.Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+        up7 = tf.keras.layers.Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            tf.keras.layers.UpSampling2D(size=(2, 2))(conv6))
+        merge7 = tf.keras.layers.Concatenate(axis=3)([conv3, up7])
+        conv7 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(
+            merge7)
+        conv7 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+        up8 = tf.keras.layers.Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            tf.keras.layers.UpSampling2D(size=(2, 2))(conv7))
+        merge8 = tf.keras.layers.Concatenate(axis=3)([conv2, up8])
+        conv8 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(
+            merge8)
+        conv8 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+        up9 = tf.keras.layers.Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
+            tf.keras.layers.UpSampling2D(size=(2, 2))(conv8))
+        merge9 = tf.keras.layers.Concatenate(axis=3)([conv1, up9])
+        conv9 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+        conv9 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        conv9 = tf.keras.layers.Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+        # 最后一层为softmax层，输出的通道为classes，对应着分类数
+        conv10 = tf.keras.layers.Conv2D(classes, 1, activation='softmax', padding='same',
+                                        kernel_initializer='he_normal')(conv9)
+
+        self.model = tf.keras.Model(inputs=inputs, outputs=conv10)
+
+    def compile(self, optimizer=tf.keras.optimizers.Adam(lr=1e-5, beta_1=0.90, beta_2=0.90),
+                loss="categorical_crossentropy",
+                metrics=['accuracy']):
+        self.model.compile(optimizer=optimizer,
+                           loss=loss,
+                           metrics=metrics)
+
+    def fit(self, x=256,
+            y=256,
+            batch_size=None,
+            epochs=10,
+            verbose=1,
+            callbacks=None,
+            validation_split=0.,
+            validation_data=None,
+            shuffle=True,
+            class_weight=None,
+            sample_weight=None,
+            initial_epoch=0,
+            steps_per_epoch=1,
+            validation_steps=None, ):
+        self.model.fit(x, y, batch_size, epochs, verbose, callbacks, validation_split, validation_data, shuffle,
+                       class_weight, sample_weight, initial_epoch, steps_per_epoch, validation_steps)
+
+    def predict(self, x, batch_size=32, verbose=1, steps=None):
+        pred = self.model.predict(x, batch_size=batch_size, verbose=verbose, steps=steps)
+        return np.argmax(pred, axis=-1)
